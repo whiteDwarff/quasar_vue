a9ddc0d9315c4421d120cd46ae65e87b1e721c47
강문호 <rkdans113@nsdevil.com>
2025-06-20 16:35:12 +0900

    no message

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,42 @@ body {
 }
 .search-wrap {
   background-color: #fafafa;
-  padding: 40px 20px;
+  padding: 25px 20px;
 }
-.search-wrap span {
+.search-wrap span.label {
   width: 15%;
   display: block;
 }
 .search-wrap .content {
   width: 80%;
   margin-left: 5%;
+}
+.edit-btn-wrap {
+  margin-top: 24px;
+  margin-bottom: 16px;
+}
+.btn {
+  padding: 8px 24px;
+}
+
+/* */
+:root {
+  --q-primary: #4f82f8;
+}
+.q-btn {
+  font-weight: 350 !important;
+  border-radius: 6px;
+}
+.q-table {
+  border-top: 2px solid rgba(0, 0, 0, 0.12);
+}
+.q-table thead tr.q-tr {
+  height: 35px;
+}
+.q-table thead tr.q-tr th {
+  padding: 5px 10px !important;
+  height: 35px;
+  background-color: #fafafa !important;
+  font-weight: 550;
 }
 </style>
--- src/pages/examInfo.vue
+++ src/pages/examInfo.vue
@@ -1,13 +1,11 @@
 <template>
   <q-page padding>
-    {{ param }}
-
     <q-card flat>
       <q-card-section class="search-wrap">
         <div class="row q-col-gutter-md">
           <div class="col-xs-12 col-sm-6 col-md-3">
             <div class="flex items-center">
-              <span>회사명</span>
+              <span class="label">회사명</span>
               <div class="content">
                 <SelectFilter v-model="param.companySeq" />
               </div>
@@ -15,15 +13,15 @@
           </div>
           <div class="col-xs-12 col-sm-6 col-md-3">
             <div class="flex items-center">
-              <span>회사명</span>
+              <span class="label">시험명</span>
               <div class="content">
-                <q-input v-model="param.companyName" outlined dense class="bg-white" />
+                <q-input v-model="param.examName" outlined dense class="bg-white" />
               </div>
             </div>
           </div>
           <div class="col-xs-12 col-sm-6 col-md-3">
             <div class="flex items-center">
-              <span>등록자</span>
+              <span class="label">등록자</span>
               <div class="content">
                 <q-input v-model="param.regId" outlined dense class="bg-white" />
               </div>
@@ -31,14 +29,59 @@
           </div>
           <div class="col-xs-12 col-sm-6 col-md-3">
             <div class="flex items-center">
-              <span>등록일</span>
+              <span class="label">등록일</span>
               <div class="content">
                 <DatePicker v-model="param.regDay" :range="true" />
               </div>
             </div>
           </div>
         </div>
+
+        <div class="flex q-mt-lg">
+          <q-space />
+          <q-btn
+            unelevated
+            color="primary"
+            label="초기화"
+            dense
+            outline
+            :ripple="false"
+            no-caps
+            class="btn q-mr-md"
+          />
+          <q-btn unelevated color="primary" label="검색" dense :ripple="false" class="btn" />
+        </div>
       </q-card-section>
+    </q-card>
+
+    <div class="flex edit-btn-wrap">
+      <q-space />
+      <q-btn unelevated color="primary" label="시험정보등록" dense :ripple="false" class="btn" />
+    </div>
+
+    <q-card flat>
+      <q-table :rows="[1, 2, 3]" flat bordered>
+        <template #header>
+          <q-tr>
+            <q-th>번호</q-th>
+            <q-th>회사명</q-th>
+            <q-th>시험명</q-th>
+            <q-th>등록자</q-th>
+            <q-th>등록일(년/월/일)</q-th>
+            <q-th>관리</q-th>
+          </q-tr>
+        </template>
+        <template #body>
+          <q-tr>
+            <q-td></q-td>
+            <q-td></q-td>
+            <q-td></q-td>
+            <q-td></q-td>
+            <q-td></q-td>
+            <q-td></q-td>
+          </q-tr>
+        </template>
+      </q-table>
     </q-card>
   </q-page>
 </template>
@@ -46,7 +89,7 @@
 <script setup>
 const param = ref({
   companySeq: '',
-  companyName: '',
+  examName: '',
   regId: '',
   regDay: [],
   regStDt: null,
